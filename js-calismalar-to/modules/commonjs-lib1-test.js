e11fba08e5d521bee1283b00a63e3fc8a177a8ff
Tuncay_hp <tunc270@gmail.com>
2024-10-13 21:05:38 +0300

    up

---
+++ js-calismalar-to/modules/commonjs-lib1-test.js
@@ -0,0 +1,4 @@
+const { add, subtract } = require("./commonjs-lib1");
+
+console.log(add(5, 5)); // 10
+console.log(subtract(10, 5)); // 5
---
+++ js-calismalar-to/modules/eslib1-test.mjs
@@ -0,0 +1,4 @@
+import { add, subtract } from "./eslib1.mjs";
+
+console.log(add(5, 5)); // 10
+console.log(subtract(10, 5)); // 5
