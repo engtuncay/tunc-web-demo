7e41f60b6ee8e62deb1029ac008c8522a110ff63
TuncayHp2 <tunc270@gmail.com>
2025-11-13 18:08:04 +0300

    up

---
+++ js/b-promise-async/promise/es6-prom1 copy.js
@@ -0,0 +1,23 @@
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    console.log("promise çalıştı");
+    //setTimeout(() => {
+    resolve([
+      { username: 'john', email: 'john@test.com' },
+      { username: 'jane', email: 'jane@test.com' },
+    ]);
+    //}, 1);
+  });
+}
+
+const promise = getUsers();
+
+promise.then((users) => {
+  console.log(users);
+});
+
+console.log("son satır");
+console.log("son satır2");
+setTimeout(()=>{
+console.log("gecikmeli son satır");
+},500);
