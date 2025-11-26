5fd95e42cdbdb47706d3dae87fb8c2c6431a9b25
TuncayHp2 <tunc270@gmail.com>
2025-11-26 22:45:55 +0300

    up

--- js/b-promise-async/promise/tor-prom1.js
+++ js/b-promise-async/promise/tor-prom1.js
@@ -10,15 +10,28 @@ function getUsers() {
   });
 }
 
+// callback =  (rs,rj) => {
+// success result, call rs
+// rs(data);
+// failure result, call rj
+// rj(err);
+// }
+// new Promise (callback);
+
 const promise = getUsers();
 
-promise.then((users) => {
+promise.then(
+  // promise tanımı içinde
+  // resolve fonksiyonuna gönderilen argümanı (users) alır
+  (users) => {
   console.log(users);
 });
 
 promise.then((users) => {
   console.log(users.length);
 });
+
+// promise.then(resolve, reject) şeklinde de kullanılabilir
 
 console.log("son satır");
 console.log("son satır2");
